0cbd920f4850d543aeff328b7d4394a6a8357d38
Alexander Matson <alexmat2on@protonmail.com>
2018-11-14 16:06:58 -0500

    Set up React with Webpack and Babel

---
+++ frontend/webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'public');
+var APP_DIR = path.resolve(__dirname, 'app');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ['babel-loader']
+    }]
+  },
+
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+
+  devServer: {
+    inline: true,
+    contentBase: './public',
+    port: 8090
+  }
+};
+
+module.exports = config;
