d341b81b824ff2aee17aa6260d76f05721afdfc1
Alexander Matson <alexmat2on@protonmail.com>
2018-11-20 17:55:34 -0500

    Fix empty word list bug

--- frontend/app/Editor.jsx
+++ frontend/app/Editor.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Editor extends React.Component {
   state = {
@@ -7,8 +7,7 @@ class Editor extends React.Component {
     locked: true,
     lockedBy: "alexmatson",
     words: [
-      { lineNum: 0, content: "test"},
-      { lineNum: 1, content: "default"}
+      { lineNum: 0, content: ""}
     ]
   }
 
@@ -16,7 +15,10 @@ class Editor extends React.Component {
     console.log("The key is", e);
 
     var { words, locked } = this.state;
-    var typingWord = words[words.length - 1].content;
+    var typingWord = "";
+
+    if (words.length > 0)
+      typingWord = words[words.length - 1].content;
 
     console.log(!locked);
 
@@ -24,34 +26,41 @@ class Editor extends React.Component {
       // If the document is unlocked...
       let currentWords = words;
 
-      if (e.keyCode == 13 || e.keyCode == 32) {
+      if (e.key == "Enter" || e.key == " ") {
         // If the user presses spacebar or enter, create a new empty line.
-        let newID = words[words.length - 1].lineNum + 1;
-        this.setState({
-          words: [...currentWords, {lineNum: newID, content: ""}]
-        });
+        let newID = 0;
 
-      } else if (e.keyCode == 8) {
+        if (words.length > 0)
+          newID = words[words.length - 1].lineNum + 1;
+
+        currentWords.push({lineNum: newID, content: ""});
+        this.setState({ words: currentWords });
+
+      } else if (e.key == "Backspace") {
         // If the user presses backspace, begin deleting characters.
 
         typingWord = typingWord.substr(0, typingWord.length - 1);
-        currentWords[words.length - 1].content = typingWord;
 
-        if (typingWord == "")
-          currentWords = words.slice(0, words.length - 1);
+        if (words.length > 0) {
+          currentWords[words.length - 1].content = typingWord;
+
+          if (typingWord == "")
+            currentWords = words.slice(0, words.length - 1);
+        }
 
         this.setState({ words: currentWords });
       } else {
+        console.log("The typing word", typingWord);
         // Otherwise, append new characters to the latest line.
         if (e.keyCode >= 33 && e.keyCode <= 126) // Only detect ASCII keyboard symbols a-z, A-Z, 0-9, and !,$,#, etc
           typingWord += e.key;
 
-        currentWords[words.length - 1].content = typingWord;
+        if (words.length > 0)
+          currentWords[words.length - 1].content = typingWord;
+        else
+          currentWords.push({lineNum: 0, content: typingWord});
 
-        this.setState({
-          // Append the new word to the word list
-          words: currentWords
-        });
+        this.setState({ words: currentWords });
       }
     }
   }
@@ -70,40 +79,12 @@ class Editor extends React.Component {
 
     if (locked) {
       // If the document is changing from locked to unlocked.
-
-      let currentWords = words;
-
       this.setState({locked: false});
+      document.getElementById("lock-btn").blur();
     } else {
       // Do this when the document is changing from unlocked to locked.
-
-      var { words } = this.state;
-      let lastID = words[words.length - 1].lineNum;
-
       this.setState({ locked: true });
-    }
-  }
-
-  handleTyping = (e) => {
-    var kc = e.keyCode;
-
-    // "Enter" or "Space" button was typed
-    if (kc == 32 || kc == 13) {
-      // Conditional is too slow, sometimes a space and extra letter will get through
-      // Thus, split the input by space to make sure only one word is assigned per line.
-      var input = e.target.value.split(" ");
-      var words = this.state.words;
-
-      let newID = words[words.length - 1].lineNum + 1;
-      let currentWords = words;
-      this.setState({
-        // Append the new word to the word list
-        words: [...currentWords, { lineNum: newID, content: input[0] }]
-      });
-
-      // If the event captured some extra characters, return them to the text box.
-      // Otherwise, wipe it out.
-      e.target.value = input[1] || "";
+      document.getElementById("lock-btn").blur();
     }
   }
 
@@ -118,25 +99,27 @@ class Editor extends React.Component {
       return <div onClick={ this.clickLine } key={ word.lineNum } id={ word.lineNum } className="edit-line">{ word.content }<br/></div>
     })
 
-    return (<div className="editor">
-      <div className="doc-header">
-        <h1>{ title }</h1>
-        <h2>{ owner }</h2>
-        <p>Status: { locked ? "Locked" : "Unlocked" }</p>
-      </div>
+    return (
+      <div className="editor">
+        <div className="doc-header">
+          <h1>{ title }</h1>
+          <h2>{ owner }</h2>
+          <p>Status: { locked ? "Locked" : "Unlocked" }</p>
+        </div>
 
-      <div className="editor-composer" id="editor-composer">
-        {wordList}
-      </div>
+        <div className="editor-composer" id="editor-composer">
+          { wordList }
+        </div>
 
-      <div className="editor-buttons">
-        <button>Save</button>
-        <button onClick={ this.handleUnlock }>
-          { locked ? "Unlock" : "Lock" }
-        </button>
-        <button>Complain</button>
+        <div className="editor-buttons">
+          <button>Save</button>
+          <button id="lock-btn" onClick={ this.handleUnlock }>
+            { locked ? "Unlock" : "Lock" }
+          </button>
+          <button>Complain</button>
+        </div>
       </div>
-    </div>)
+    )
   }
 }
 
