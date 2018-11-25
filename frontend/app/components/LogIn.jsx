863ca2ef3896a4c91d960c8391a17fb6b4e691f9
Alexander Matson <alexmat2on@protonmail.com>
2018-11-24 22:25:20 -0500

    Create login form

--- frontend/app/components/LogIn.jsx
+++ frontend/app/components/LogIn.jsx
@@ -2,9 +2,25 @@ import React, { Component } from 'react';
 
 class LogIn extends React.Component {
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  }
+
   render() {
     return (
-      "This is the login page"
+      <div>
+        <h2>Sign In</h2>
+        <hr />
+        <form onSubmit={ this.handleSubmit }>
+          <label htmlFor="username">Username</label><br />
+          <input type="text" name="username" /><br />
+
+          <label htmlFor="password">Pass</label><br />
+          <input type="password" name="password" /><br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     )
   }
 }
--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -2,13 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
-
 // fetch("http://localhost:5000/helloWorld").then(res => {
 //   return res.json();
 // }).then(myjson => {
 //   console.log(myjson);
 // })
+
+// Since our app will use HTML5 LocalStorage API, and not all browsers support it,
+// only load the app if the localstorage object is available to us.
+if (storageAvailable('localStorage')) {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+  );
+}
+else {
+  ReactDOM.render(
+    <p>Local storage is unsupported</p>,
+    document.getElementById('app')
+  );
+}
+
+function storageAvailable(type) {
+    try {
+        var storage = window[type],
+            x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            storage.length !== 0;
+    }
+}
