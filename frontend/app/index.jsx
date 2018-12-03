0c22041d3ba7c58604e98a809dbedbef716c5e5a
kelly lu <klu001@citymail.cuny.edu>
2018-12-03 14:00:02 -0500

    add comment

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import Editor from './components/Editor/Editor';
 
 ReactDOM.render(
-  <App />,
+  <Editor />, //to see editor, change out Editor with App to see homepage
   document.getElementById('app')
 );
 
