816e8323767869b6fc8c1dac9facba5fbc344b3a
Alexander Matson <alexmat2on@protonmail.com>
2018-12-06 18:06:26 -0500

    add CI config

--- frontend/app/Config.js
+++ frontend/app/Config.js
@@ -1,6 +1,9 @@
 // Define the URL of the backend server.
 
-const API_BASE_URL = "http://localhost:5000/api";
+const DEV_URL = "http://localhost:5000/api";
+const PROD_URL = "http://206.189.193.154/api/";
+
+const API_BASE_URL = PROD_URL;
 
 module.exports = {
   API_BASE_URL
