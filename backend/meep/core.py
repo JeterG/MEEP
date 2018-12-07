d1e319aea411710324e422940576657ac9f551f6
Jeter <35966325+gutierrezjeter@users.noreply.github.com>
2018-12-07 10:32:54 -0500

    Added Document Ids

--- backend/meep/core.py
+++ backend/meep/core.py
@@ -6,9 +6,10 @@ tabooList = ["EVIL", "LIAR"]
 allDocuments = []
 allUsers = []
 uniqueIdUsers = -1
+uniqueIdDocuments = -1
 
 
-def blocked(User):  # Global functions are functions that are handled or necessary for the system
+def blocked(User):  # Blocked function to check wheter a user can do anything or if they have to fix a document
     if (User._blocked == True):
         print("Update document before you continue")
     else:
@@ -21,7 +22,7 @@ def timeStamp():
 
 class SuperUser:
 
-    def __init__(self, name,password, interests):
+    def __init__(self, name, password, interests):
         global uniqueIdUsers
         uniqueIdUsers += 1
         self._membership = str.upper("Super")
@@ -39,8 +40,8 @@ class SuperUser:
     def promote(self, user):
         if str.upper(user._membership) == "GUEST":
             user._membership = "ORDINARY"
-            user._password=user._application[0]
-            user._interests=str.upper(user._application[1])
+            user._password = user._application[0]
+            user._interests = str.upper(user._application[1])
             del user._application
             user._ownedDocuments = []
             return
@@ -114,14 +115,14 @@ class GuestUser:
         allUsers.append(self)
         return
 
-    def applyToOrdinary(self,password,interests):
-        self._application=[password,interests]
+    def applyToOrdinary(self, password, interests):
+        self._application = [password, interests]
         self._requestPromotion = 1
         return
 
 
 class OrdinaryUser:
-    def __init__(self, name,password,interests):
+    def __init__(self, name, password, interests):
         global uniqueIdUsers
         uniqueIdUsers += 1
         self._membership = str.upper("ORDINARY")
@@ -138,6 +139,8 @@ class OrdinaryUser:
 
 class Document:
     def __init__(self, documentName, User):
+        global uniqueIdDocuments
+        uniqueIdDocuments += 1
         self._lock = False
         self._documentName = documentName
         self._owner = User._username
@@ -145,24 +148,25 @@ class Document:
         self._unlockedBy = ""
         self._users = [User._username]
         self._documentBody = []
-        self._versionHistory = [(0,self._documentBody.copy(),self._owner,timeStamp())]
+        self._id = uniqueIdDocuments
+        self._versionHistory = [(0, self._documentBody.copy(), self._owner, timeStamp())]
         allDocuments.append(self)
 
     def unlockDocument(self,
                        User):  # Unlock the document, only the super user can unlock the document regardless of who locked it
         # otherwise document can be unlocked by whoever locked it or the owner
-        if ((str.upper(User._membership) == "SUPER") & self._lock == True):
+        if ((str.upper(User._membership) == "SUPER") & self._lock == True):#unlock the document if you are super user
             self._lock = False
             self._unlockedBy = User._username
             self._lockedBy = ""
         else:
-            if self._lock == True:
+            if self._lock == True:#unlock the document if you are the owner
                 if self._owner == User._username:
                     self._lock = False
                     self._unlockedBy = User._username
                     self._lockedBy = ""
                 else:
-                    if self._lockedBy == User._username:
+                    if self._lockedBy == User._username:#if you are not the owner then unlock the document if you locked it initially
                         self._lock = False
                         self._unlockedBy = User._username
                         self._lockedBy = ""
@@ -172,7 +176,7 @@ class Document:
                 print(self._documentName, " is not Locked")
                 return
 
-    def lockDocument(self, User):
+    def lockDocument(self, User):#lock the document that can be done by anyone
         if (self._lock == False):
             self._lock = True
             self._lockedBy = User._username
@@ -202,16 +206,17 @@ class Document:
         else:
             return
 
-    def add(self, Word,User):
+    def add(self, Word, User):
         self._documentBody.append(Word)
         su.applyTabooList()
-        self._versionHistory.append((len(self._versionHistory),self._documentBody.copy(),User._username,timeStamp()))
+        self._versionHistory.append((len(self._versionHistory), self._documentBody.copy(), User._username, timeStamp()))
         return
 
-    def delete(self, index,User):
-        if len(self._documentBody)>=index:
+    def delete(self, index, User):
+        if len(self._documentBody) >= index:
             del self._documentBody[index]
-            self._versionHistory.append((len(self._versionHistory),self._documentBody.copy(),User._username,timeStamp()))
+            self._versionHistory.append(
+                (len(self._versionHistory), self._documentBody.copy(), User._username, timeStamp()))
         else:
             return
 
@@ -220,10 +225,10 @@ class Document:
     # def denyInvitation():
 
 
-su = SuperUser("su", "root" ,"Minecraft,Algorithms,Pokemon")
+su = SuperUser("su", "root", "Minecraft,Algorithms,Pokemon")
 Doc1 = Document("Doc1", su)
 print("locked by", Doc1._lockedBy, sep=",")
-Mik = OrdinaryUser("Mik", "password","Cheese")
+Mik = OrdinaryUser("Mik", "password", "Cheese")
 Doc1.lockDocument(Mik)
 Doc1.unlockDocument(su)
 print(Mik._membership)
@@ -238,12 +243,12 @@ print(Doc1._documentBody)
 su.applyTabooList()
 su.updateTabooList("bob")
 Doc1.lockDocument(su)
-Doc1.add("Bob",su)
-Doc1.add("LIAR",su)
-Doc1.add("test",su)
-Doc1.add("case",su)
-Doc1.add("trial",su)
-Doc1.add("fail",su)
+Doc1.add("Bob", su)
+Doc1.add("LIAR", su)
+Doc1.add("test", su)
+Doc1.add("case", su)
+Doc1.add("trial", su)
+Doc1.add("fail", su)
 
 # Doc1.delete("UNK")
 print(Doc1._documentBody)
@@ -270,7 +275,7 @@ Doc1 = allDocuments[0]
 print(Doc1._documentBody)
 for document in allDocuments:
     globals()[document._documentName] = document
-    print("hello",document)
+    print("hello", document)
 print(Doc1._documentBody, "Is the document", sep=" ")
 # Doc1._documentBody = []
 Doc3 = Doc1
@@ -285,17 +290,18 @@ print(su._id)
 print(Mik._id)
 print(timeStamp())
 print(Doc1._documentBody)
-Doc1.add("Hello",su)
+Doc1.add("Hello", su)
 for version in Doc1._versionHistory:
-    print("Version",Doc1._versionHistory.index(version), " is ",version)
+    print("Version", Doc1._versionHistory.index(version), " is ", version)
 # print(Doc1._versionHistory.length())
 if globals()["su"] in allUsers:
-    print ("hello")
-print (len(allUsers))
-Doc1.delete(2,su)
-print("Version",len(Doc1._versionHistory)-1," is ", Doc1._versionHistory[len(Doc1._versionHistory)-1])
+    print("hello")
+print(len(allUsers))
+Doc1.delete(2, su)
+print("Version", len(Doc1._versionHistory) - 1, " is ", Doc1._versionHistory[len(Doc1._versionHistory) - 1])
 
-Bob=GuestUser("Bob")
-Bob.applyToOrdinary("password","Minecraft")
+Bob = GuestUser("Bob")
+Bob.applyToOrdinary("abc123", "Minecraft")
 print(Bob._application)
 su.updateMembership(Bob)
+print(Bob._password)
