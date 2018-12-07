7ac67d4e992d8f125e9f2caef7ef48256a6267b3
Alexander Matson <alexmat2on@protonmail.com>
2018-12-06 19:06:46 -0500

    modify build script

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd frontend/
-/root/.nvm/versions/node/v11.3.0/lib/node_modules/npm run build
-
+/root/.nvm/versions/node/v11.3.0/bin/npm run build
 cp public/* /var/www/html/
 
 cd ../backend
-gunicorn --bind 0.0.0.0:5000 wsgi:app
+killall screen
+/usr/bin/screen -d -m /usr/bin/gunicorn --bind 0.0.0.0:5000 wsgi:app
