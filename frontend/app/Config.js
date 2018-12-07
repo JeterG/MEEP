3b14d06c18649f61fec44eb5349e06816f4b08f8
Alexander Matson <alexmat2on@protonmail.com>
2018-12-06 20:04:50 -0500

    fix config

--- frontend/app/Config.js
+++ frontend/app/Config.js
@@ -1,7 +1,7 @@
 // Define the URL of the backend server.
 
 const DEV_URL = "http://localhost:5000/api";
-const PROD_URL = "http://206.189.193.154/api/";
+const PROD_URL = "http://206.189.193.154/api";
 
 const API_BASE_URL = PROD_URL;
 
