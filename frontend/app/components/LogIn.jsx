493f8c21f595743718dfd420c73ba21f2e45e2e2
JAePortfolio <jarena000@citymail.cuny.edu>
2018-12-11 18:12:01 -0500

    Added Navbar. Styled Home Page. Fixed Styling Issue on Login/Register

--- frontend/app/components/LogIn.jsx
+++ frontend/app/components/LogIn.jsx
@@ -38,37 +38,37 @@ class LogIn extends React.Component {
       <div className="card-panel light-blue accent-1">
         <div className="container">
           <div className="center-align">
-            <img src="/images/logo.png" />
-          <div>
-            <h3>Sign In</h3>
-            <hr />
-            <div id="message">
-              {this.state.message}
+            <img src="/images/logo.png" style={{cursor: 'pointer'}}/>
+            <div>
+              <h3>Sign In</h3>
+              <hr />
+              <div id="message">
+                {this.state.message}
+              </div>
+              <form onSubmit={ this.handleSubmit }>
+                <div className="row">
+                  <div className="input-field col s6 offset-m3">
+                    <i className="material-icons prefix">account_circle</i>
+                    <input id="username" type="text" className="validate" />
+                    <label htmlFor="username">Username</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="input-field col s6 offset-m3">
+                  <i className="material-icons prefix">lock</i>
+                    <input id="password" type="password" className="validate" />
+                    <label htmlFor="password">Password</label>
+                  </div>
+                </div>
+                <button className="btn waves-effect waves-light" type="submit" name="action">Submit
+                  <i className="material-icons right">send</i>
+                  </button>
+                {/*<button type="submit">Submit</button> */}
+                </form>
+              </div>
             </div>
-            <form onSubmit={ this.handleSubmit }>
-              <div className="row">
-                <div className="input-field col s6 offset-m3">
-                  <i className="material-icons prefix">account_circle</i>
-                  <input id="username" type="text" className="validate" />
-                  <label htmlFor="username">Username</label>
-                </div>
-              </div>
-              <div className="row">
-                <div className="input-field col s6 offset-m3">
-                <i className="material-icons prefix">lock</i>
-                  <input id="password" type="password" className="validate" />
-                  <label htmlFor="password">Password</label>
-                </div>
-              </div>
-              <button className="btn waves-effect waves-light" type="submit" name="action">Submit
-                <i className="material-icons right">send</i>
-                </button>
-              {/*<button type="submit">Submit</button> */}
-              </form>
+          </div>
         </div>
-      </div>
-    </div>
-    </div>
     )
   }
 }
